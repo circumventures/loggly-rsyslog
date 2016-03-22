21a9bd9d10ee09b86271b0527fa35793530c061f
Tony F <tony@circumventures.com>
2016-03-22 10:05:02 -0700

    incd version

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name             'loggly-rsyslog'
-maintainer       'Matt Veitas'
-maintainer_email 'mveitas@gmail.com'
+maintainer       'Anthony Faoro'
+maintainer_email 'tony@circumventures.com'
 license          'Apache 2.0'
 description      'Configures rsyslog to send logs to Loggly'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.1.1'
+version          '2.1.2'
 
 supports 'ubuntu', '>= 12.04'
 
