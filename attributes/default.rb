2aac65f557ffe6483d6043cb94f4b1b83ba54d59
Tony F <tony@circumventures.com>
2016-03-30 14:39:10 -0400

    they want the entire checksum now i guess

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,7 +6,7 @@ default['loggly']['log_dirs'] = []
 default['loggly']['tls']['enabled'] = true
 default['loggly']['tls']['cert_path'] = '/etc/rsyslog.d/keys/ca.d'
 default['loggly']['tls']['cert_url'] = 'https://logdog.loggly.com/media/logs-01.loggly.com_sha12.crt'
-default['loggly']['tls']['cert_checksum'] = 'b562ae82b54b'
+default['loggly']['tls']['cert_checksum'] = 'b562ae82b54bcb43923290e78949153c0c64910d40b02d2207010bb119147ffc'
 # default['loggly']['tls']['intermediate_cert_url'] = 'https://certs.secureserver.net/repository/sf_bundle.crt'
 # default['loggly']['tls']['intermediate_cert_checksum'] = '87d171b3333c'
 
