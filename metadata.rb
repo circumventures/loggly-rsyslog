f553319743450a2d6cbf5136001f423a97f33a88
Tony F <tony@circumventures.com>
2017-04-04 21:22:56 -0700

    ancient version eh?

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,8 @@ maintainer_email 'tony@circumventures.com'
 license          'Apache 2.0'
 description      'Configures rsyslog to send logs to Loggly'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.1.2'
+version          '2.1.3'
 
 supports 'ubuntu', '>= 12.04'
 
-depends "rsyslog", "~> 1.5.0"
-
+depends 'rsyslog' # , "~> 1.5.0"
