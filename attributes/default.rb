4ac397290d7c46db0a2e1b0ede85984bb2b5e231
Tony F <tony@circumventures.com>
2017-04-04 21:20:37 -0700

    no, thats ridiculous

--- attributes/default.rb
+++ attributes/default.rb
@@ -25,5 +25,3 @@ default['loggly']['rsyslog']['input_file_poll_interval'] = 10
 default['loggly']['rsyslog']['workdirectory'] = '/var/spool/rsyslog'
 default['loggly']['rsyslog']['actionqueuefilename'] = node.name
 default['loggly']['rsyslog']['actionqueuemaxdiskspace'] = '1g'
-
-override['rsyslog']['repeated_msg_reduction'] = 'off'
